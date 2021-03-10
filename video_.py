7ffa8fe356a100b2481ac2eac89c724a0a202772
vishalbpatil1 <52704376+vishalbpatil1@users.noreply.github.com>
2021-03-10 10:18:47 +0530

    Add files via upload

---
+++ video_.py
@@ -0,0 +1,26 @@
+cap = cv2.VideoCapture(0)
+while True: 
+    __, frame = cap.read()
+    result = detector.detect_faces(frame)
+    if result != []:
+        for person in result:
+            bounding_box = person['box']
+            keypoints = person['keypoints']
+    
+            cv2.rectangle(frame,
+                          (bounding_box[0], bounding_box[1]),
+                          (bounding_box[0]+bounding_box[2], bounding_box[1] + bounding_box[3]),
+                          (0,155,255),
+                          2)
+            cv2.circle(frame,(keypoints['left_eye']), 2, (0,155,255), 2)
+            cv2.circle(frame,(keypoints['right_eye']), 2, (0,155,255), 2)
+            cv2.circle(frame,(keypoints['nose']), 2, (0,155,255), 2)
+            cv2.circle(frame,(keypoints['mouth_left']), 2, (0,155,255), 2)
+            cv2.circle(frame,(keypoints['mouth_right']), 2, (0,155,255), 2)
+    cv2.imshow('frame',frame)
+   ## print(person['keypoints']['mouth_left'])
+    key=cv2.waitKey(1)
+    if key ==ord('v'): 
+        break 
+cap.release()
+cv2.destroyAllWindows()
